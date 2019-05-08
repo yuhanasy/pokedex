60fdde1f07c7db52a95ec7ff07b1d0710bda4e7e
Yuhanas Yulianto <yulianto.yuhanas@gmail.com>
2019-05-08 15:48:23 +0700

    Add logo to navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './styles.scss'
+import pokeball from '../../assets/images/pokeball.png'
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar-wrapper">
-        <div className="logo">Logo</div>
+        <div className="logo">
+          <img className="logo-img" src={pokeball} alt="Pokedex" />
+          <div className="logo-text">Pokedex</div>
+        </div>
         <div className="menu">
           <div>Menu</div>
           <div>Search</div>
