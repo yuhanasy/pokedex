f9b57d5ae274b0053ef07035f65b765dad7a4c1f
Yuhanas Yulianto <yulianto.yuhanas@gmail.com>
2019-05-08 15:26:33 +0700

    Add first components projects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navbar from './components/navbar'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/navbar/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles.scss'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-wrapper">
+        <div className="logo">Logo</div>
+        <div className="menu">
+          <div>Menu</div>
+          <div>Search</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
